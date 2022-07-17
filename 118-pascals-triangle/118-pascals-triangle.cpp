27caa71c2a101c2106225228231453af6d4197b5
Ahmed Gamal <101002059+AhmedGamal-29@users.noreply.github.com>
2022-07-17 13:43:09 +0200

    Time: 0 ms (100.00%), Space: 6.4 MB (90.76%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> triangle(numRows);
+        for(int i=0;i<numRows;i++)
+        {
+            triangle[i].resize(i+1);
+            triangle[i][0]=triangle[i][i]=1;
+        }
+        
+        for(int i=2;i<numRows; i++){
+            
+            for(int j=1;j<i;j++){
+                
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+            }
+            
+        }
+        return triangle;
+  
+        
+    }
+};
