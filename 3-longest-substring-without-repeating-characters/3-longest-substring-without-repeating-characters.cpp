4e237106dead3174c12143905eb8fe1b326d7eb2
Ahmed Gamal <101002059+AhmedGamal-29@users.noreply.github.com>
2022-08-08 14:33:29 +0200

    Time: 37 ms (36.43%), Space: 13.4 MB (15.06%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,26 @@
 class Solution 
 {
 public:
-        
-    
     int lengthOfLongestSubstring(string s) 
     {
         int n=s.length();
         int i=0, j=0;
         int max_len=0;
+        
+        //unique characters in set
         set <char> set_chars;
+        
         while(j< n)
         {
+            //character not in set
             if(set_chars.count(s[j])==0)
             {
                 set_chars.insert(s[j]);
                 max_len=max(max_len,(int)set_chars.size());
                 j++;
             }
+            
+            //character in set
             else
             {
                 set_chars.erase(s[i]);
