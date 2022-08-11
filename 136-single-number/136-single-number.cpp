343fba6d65c0ee7791a63be94811dbee97296c10
Ahmed Gamal <101002059+AhmedGamal-29@users.noreply.github.com>
2022-08-11 14:33:08 +0200

    Time: 48 ms (19.62%), Space: 17 MB (52.12%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i!=nums.size()-1 && nums[i]==nums[i+1])
+                i++;
+            else
+                return nums[i];
+            
+   
+            
+        }
+        return -1;
+    }
+};
